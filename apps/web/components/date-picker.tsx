7f03cbdddc68a82f3155efbc88e77653a471d91f
woong <woong@stunitas.com>
2026-04-03 15:57:19 +0900

    WIP Updating calenber layout

--- apps/web/components/date-picker.tsx
+++ apps/web/components/date-picker.tsx
@@ -1,26 +1,33 @@
 import * as React from "react"
 
+import { Button } from "@workspace/ui/components/button"
 import { Calendar } from "@workspace/ui/components/calendar"
 import {
-  SidebarGroup,
-  SidebarGroupContent,
+    SidebarGroup,
+    SidebarGroupContent,
 } from "@workspace/ui/components/sidebar"
 
 export function DatePicker() {
-  const [date, setDate] = React.useState<Date | undefined>(
-    new Date(new Date().getFullYear(), new Date().getMonth(), 12)
-  )
-  return (
-    <SidebarGroup className="px-0">
-      <SidebarGroupContent>
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          captionLayout="dropdown"
-          className="bg-transparent [--cell-size:2.1rem]"
-        />
-      </SidebarGroupContent>
-    </SidebarGroup>
-  )
+    const [date, setDate] = React.useState<Date | undefined>(
+        new Date(new Date().getFullYear(), new Date().getMonth(), 12)
+    )
+    return (
+        <SidebarGroup className="px-0">
+            <SidebarGroupContent>
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    captionLayout="dropdown"
+                    className="bg-transparent"
+                />
+
+                <div className="px-3">
+                    <Button variant="default" size="sm" className="w-full">
+                        일정 추가
+                    </Button>
+                </div>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
 }
